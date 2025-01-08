ab0a69ed0804276c31507296ef4eca025bfd411b
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2025-01-08 17:11:36 +0700

    add Entity Partner Template Rest API CURD

--- src/main/java/com/agus/ramdan/bmps/domain/Partner.java
+++ src/main/java/com/agus/ramdan/bmps/domain/Partner.java
@@ -24,4 +24,17 @@ public class Partner {
     @JsonProperty(index = 2)
     @Schema(description = "Name")
     private String name;
+
+    // Address Start
+    @Column
+    private String street1;
+    @Column
+    private String street2;
+    @Column
+    private String city;
+    @Column
+    private String zip_code;
+    @Column
+    private String country;
+    // Address End
 }
