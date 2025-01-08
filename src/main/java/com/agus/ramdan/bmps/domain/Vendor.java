cd89453bda976627c5ea6800647eff3d7a80db55
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2025-01-08 10:54:09 +0700

    Vendor

---
+++ src/main/java/com/agus/ramdan/bmps/domain/Vendor.java
@@ -0,0 +1,27 @@
+package com.agus.ramdan.bmps.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "t_vendor")
+@Schema
+public class Vendor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dm_sec_vendor_gen")
+    @SequenceGenerator(name = "dm_sec_vendor_gen" , sequenceName = "dm_sec_vendor")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    @JsonProperty(index = 2)
+    @Schema(description = "Name")
+    private String name;
+}
---
+++ src/main/java/com/agus/ramdan/bmps/repository/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.agus.ramdan.bmps.repository;
+
+import com.agus.ramdan.bmps.domain.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+}
