0666dbe6e67edfe8fa841cf6b94eae220ca95974
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2025-01-08 17:29:17 +0700

    add Entity Service Location Template Rest API CURD

---
+++ src/main/java/com/agus/ramdan/bmps/domain/ServiceLocation.java
@@ -0,0 +1,45 @@
+package com.agus.ramdan.bmps.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "t_service_location")
+@Schema
+public class ServiceLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dm_sec_service_location_gen")
+    @SequenceGenerator(name = "dm_sec_service_location_gen" , sequenceName = "dm_sec_service_location")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    @JsonProperty(index = 2)
+    @Schema(description = "Name")
+    private String name;
+
+    // Address Start
+    @Column
+    private String street1;
+    @Column
+    private String street2;
+    @Column
+    private String city;
+    @Column
+    private String zip_code;
+    @Column
+    private String country;
+    // Address End
+
+    @Column
+    private Float longitude ;
+    @Column
+    private Float latitude;
+}
