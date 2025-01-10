713806f777a603484f0b57536d55a17cb7532881
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2025-01-10 14:52:47 +0700

    add vendor crew

---
+++ src/main/java/com/agus/ramdan/bmps/domain/VendorCrew.java
@@ -0,0 +1,34 @@
+package com.agus.ramdan.bmps.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "t_vendor_crew")
+@Schema
+public class VendorCrew {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dm_sec_vendor_crew_gen")
+    @SequenceGenerator(name = "dm_sec_vendor_crew_gen" , sequenceName = "dm_sec_vendor_crew")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    @JsonProperty(index = 2)
+    @Schema(description = "Name")
+    private String name;
+
+    @Column
+    private String nik;
+
+    @Column
+    private String ktp;
+
+}
---
+++ src/main/java/com/agus/ramdan/bmps/repository/VendorCrewRepository.java
@@ -0,0 +1,7 @@
+package com.agus.ramdan.bmps.repository;
+
+import com.agus.ramdan.bmps.domain.VendorCrew;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorCrewRepository extends JpaRepository<VendorCrew, Long> {
+}
