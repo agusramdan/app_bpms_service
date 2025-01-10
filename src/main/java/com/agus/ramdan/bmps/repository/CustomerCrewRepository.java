69181d36bb8731b285579dccbeead185a84dcb79
Agus Muhammad Ramdan <agus.ramdan@smartdata-solusi.com>
2025-01-10 17:11:14 +0700

    update CustomerCrew

---
+++ src/main/java/com/agus/ramdan/bmps/repository/CustomerCrewRepository.java
@@ -0,0 +1,7 @@
+package com.agus.ramdan.bmps.repository;
+
+import com.agus.ramdan.bmps.domain.CustomerCrew;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerCrewRepository extends JpaRepository<CustomerCrew, Long> {
+}
