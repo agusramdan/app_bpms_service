8ab6c538a50dcd6109c81ee17c82917d83f0320d
Agus Muhammad Ramdan <agus@antarejasinergi.com>
2025-02-02 09:31:36 +0700

    Zipkin integration
    Signed-off-by: Agus Muhammad Ramdan <agus@antarejasinergi.com>

--- src/main/java/com/agus/ramdan/bmps/exception/Errors.java
+++ src/main/java/com/agus/ramdan/bmps/exception/Errors.java
@@ -10,6 +10,8 @@ import java.util.Date;
 public class Errors {
     private final Date timestamp;
     private final String message;
+    private final String trace_id;
+    private final String span_id;
     private final String details;
     private final ErrorValidation[] errors;
 }
--- src/main/java/com/agus/ramdan/bmps/exception/GlobalExceptionHandler.java
+++ src/main/java/com/agus/ramdan/bmps/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.agus.ramdan.bmps.exception;
 
 import java.util.Date;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Tracer;
 import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +15,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private final Tracer tracer;
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+        String traceId = tracer.currentSpan() != null ? tracer.currentSpan().context().traceId() : "N/A";
+        String spanId = tracer.currentSpan() != null ? tracer.currentSpan().context().spanId() : "N/A";
+        val error = new Errors(new Date(), ex.getMessage(),traceId,spanId,request.getDescription(false),null);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> badRequestException(BadRequestException ex, WebRequest request) {
-        val error = new Errors (new Date(), ex.getMessage(), request.getDescription(false),ex.getErrors());
+        String traceId = tracer.currentSpan() != null ? tracer.currentSpan().context().traceId() : "N/A";
+        String spanId = tracer.currentSpan() != null ? tracer.currentSpan().context().spanId() : "N/A";
+        val error = new Errors(new Date(), ex.getMessage(),traceId,spanId,request.getDescription(false),ex.getErrors());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
@@ -34,7 +42,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
         logger.error(ex.getMessage(),ex);
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        String traceId = tracer.currentSpan() != null ? tracer.currentSpan().context().traceId() : "N/A";
+        String spanId = tracer.currentSpan() != null ? tracer.currentSpan().context().spanId() : "N/A";
+        val error = new Errors(new Date(), ex.getMessage(),traceId,spanId, request.getDescription(false),null);
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
